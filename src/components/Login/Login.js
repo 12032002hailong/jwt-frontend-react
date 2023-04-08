9b4cd2760d12b8fab7dd6fa5327977ce0e5924f3
Hai Longe <hailongkc1203@example.com>
2023-04-08 21:42:28 +0700

    Form Login Responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,29 +1,35 @@
 import React from "react";
-import './Login.scss'
+import "./Login.scss";
 
 const Login = (props) => {
   return (
-    <div className="login-container mt-3">
+    <div className="login-container">
       <div className="container">
-        <div className="row">
-          <div className="content-left col-7">
-            <div className="brand">
-              Hai Long
-            </div>
-            <div className="detail">
-            Learning everything
-            </div>
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+            <div className="brand">Hai Long</div>
+            <div className="detail">Learning everything</div>
           </div>
-          <div className="content-right col-5 d-flex flex-column gap-3 py-3">
-            <input type="text" className="form-control" placeholder="Email address or phone number"/>
-            <input type="password" className="form-control" placeholder="Password"/>
+          <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+          <div className="brand d-sm-none">Hai Long</div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email address or phone number"
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
             <button className="btn btn-primary">Login</button>
-            <span className="text-center">Forgot your password?</span>
-            <hr/>
+            <span className="text-center">
+              <a className="forgot-password" href="#"> Forgot your password?</a>
+            </span>
+            <hr />
             <div className="text-center">
-            <button className="btn btn-info">Create a new account</button>
+              <button className="btn btn-success">Create a new account</button>
             </div>
-           
           </div>
         </div>
       </div>
